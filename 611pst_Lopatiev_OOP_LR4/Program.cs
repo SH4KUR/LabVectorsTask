e4fa8ab9db48e68bcac994212088d37816e166a9
SH4KUR <40540759+SH4KUR@users.noreply.github.com>
2019-11-15 21:28:49 +0200

    Добавьте файлы проекта.

---
+++ 611pst_Lopatiev_OOP_LR4/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _611pst_Lopatiev_OOP_LR4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            do
+            {
+                Vector vectorA = new Vector(SetLengthVector("A"));
+                Vector vectorB = new Vector(SetLengthVector("B"));
+
+                Vector vectorX = null;
+
+                if (vectorA.GetLengthVector() == vectorB.GetLengthVector())
+                {
+#if DEBUG
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"X = -10");
+    Console.ForegroundColor = ConsoleColor.White;
+#endif
+                    vectorX = -10;
+                }
+                else
+                {
+                    try
+                    {
+                        if (vectorA.GetLengthVector() > vectorB.GetLengthVector())
+                        {
+#if DEBUG
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"X = 1 - A / B");
+    Console.ForegroundColor = ConsoleColor.White;
+#endif
+                            vectorX = 1 - vectorB / vectorA;
+
+                        }
+                        else
+                        {
+#if DEBUG
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"X = (A - 52) / B");
+    Console.ForegroundColor = ConsoleColor.White;
+#endif
+                            vectorX = (vectorA - 52) / vectorB;
+
+                        }
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Деление на ноль запрещенно!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                Console.WriteLine("\n\tРезультаты:\n");
+
+                Console.WriteLine($"A: {vectorA.GetVectorString()}");
+                Console.WriteLine($"B: {vectorB.GetVectorString()}");
+                Console.WriteLine($"\nX: {vectorX?.GetVectorString() ?? "не задано"}");
+
+                exit = ExitProgram();
+
+            } while (exit == false);
+
+            Console.Write("\nНажмите ENTER: ");
+            Console.ReadLine();
+        }
+
+        private static int SetLengthVector(string nameVector)
+        {
+            int lengthVector;
+            do
+            {
+                do
+                {
+                    Console.Write($"Введите размер вектора {nameVector}: ");
+                } while (!int.TryParse(Console.ReadLine(), out lengthVector));
+            } while (lengthVector <= 0);
+
+            return lengthVector;
+        }
+
+        public static bool ExitProgram()
+        {
+            Console.WriteLine("\n" + new string('*', 13));
+            Console.WriteLine("Посчитать еще раз? (y / n)");
+
+            string answer;
+
+            do
+            {
+                Console.Write("Ответ: ");
+                answer = Console.ReadLine();
+            }
+            while (!(answer.Equals("y") || answer.Equals("n")));
+
+            return answer.Equals("n");
+        }
+    }
+}
---
+++ 611pst_Lopatiev_OOP_LR4/Vector.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _611pst_Lopatiev_OOP_LR4
+{
+    class Vector
+    {
+        private int[] _arr;
+
+        public Vector(int countArr)
+        {
+            _arr = new int[countArr];
+
+            if (countArr > 10)
+                SetRandomArray();
+            else
+                SetArray();
+        }
+
+        private Vector()
+        {
+
+        }
+
+        private void SetArray()
+        {
+            for (var i = 0; i < _arr.Length; i++)
+            {
+                do
+                {
+                    Console.Write($"ARR[{i + 1}] = ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out _arr[i]));
+            }
+        }
+
+        private void SetRandomArray()
+        {
+            Random rand = new Random();
+
+            for (var i = 0; i < _arr.Length; i++)
+            {
+                _arr[i] = rand.Next(0, 100);
+            }
+
+#if DEBUG
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("vector = " + String.Join(" ", _arr));
+    Console.ForegroundColor = ConsoleColor.White;
+#endif
+        }
+
+        public int GetLengthVector()
+        {
+            return _arr.Length;
+        }
+
+        public string GetVectorString()
+        {
+            return String.Join(" ", _arr);
+        }
+
+        public static bool operator >(Vector vectorX, Vector vectorY) => vectorX._arr.Max() >= vectorY._arr.Max();
+
+        public static bool operator <(Vector vectorX, Vector vectorY) => vectorX._arr.Max() < vectorY._arr.Max();
+
+        public static Vector operator /(Vector vectorX, Vector vectorY)
+        {
+            List<int> newArr = new List<int>();
+
+            for (var i = 0; i < vectorX._arr.Length; i++)
+            {
+                if (i >= vectorY._arr.Length)
+                    break;
+
+                newArr.Add(vectorX._arr[i] / vectorY._arr[i]);
+#if DEBUG
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"{vectorX._arr[i]} / {vectorY._arr[i]} = {vectorX._arr[i] / vectorY._arr[i]}");
+    Console.ForegroundColor = ConsoleColor.White;
+#endif
+            }
+
+            return new Vector()
+            {
+                _arr = newArr.ToArray()
+            };
+        }
+
+        public static Vector operator -(int x, Vector vectorX) =>
+            new Vector()
+            {
+                _arr = vectorX._arr.Select(t => x - t).ToArray()
+            };
+
+        public static Vector operator -(Vector vectorX, int x) =>
+            new Vector()
+            {
+                _arr = vectorX._arr.Select(t => t - x).ToArray()
+            };
+
+        public static implicit operator Vector(int x)
+        {
+            return new Vector()
+            {
+                _arr = new[] {x}
+            };
+        }
+    }
+}
